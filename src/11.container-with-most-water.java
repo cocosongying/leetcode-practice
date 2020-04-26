ea20670d7d3d5489c5b2e9856f64a371b4e4eee2
cocosongying <cocosongying@gmail.com>
2020-04-26 17:13:18 +0800

    feat:Solution No.11

---
+++ src/11.container-with-most-water.java
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=11 lang=java
+ *
+ * [11] Container With Most Water
+ *
+ * https://leetcode.com/problems/container-with-most-water/description/
+ *
+ * algorithms
+ * Medium (49.57%)
+ * Likes:    5353
+ * Dislikes: 551
+ * Total Accepted:    596.8K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[1,8,6,2,5,4,8,3,7]'
+ *
+ * Given n non-negative integers a1, a2, ..., an , where each represents a
+ * point at coordinate (i, ai). n vertical lines are drawn such that the two
+ * endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together
+ * with x-axis forms a container, such that the container contains the most
+ * water.
+ * 
+ * Note: You may not slant the container and n is at least 2.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In
+ * this case, the max area of water (blue section) the container can contain is
+ * 49. 
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ */
+
+// @lc code=start
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1, result = 0;
+        while (left <= right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            result = Math.max(result, area);
+            if (height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return result;
+    }
+}
+// @lc code=end
+
