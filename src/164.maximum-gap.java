8643fb5a926d66234017ceaf3cb65c29be894b38
cocosongying <cocosongying@gmail.com>
2020-06-05 10:40:20 +0800

    feat:Solution No.160,162,164,165

---
+++ src/164.maximum-gap.java
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode id=164 lang=java
+ *
+ * [164] Maximum Gap
+ *
+ * https://leetcode.com/problems/maximum-gap/description/
+ *
+ * algorithms
+ * Hard (34.76%)
+ * Likes:    780
+ * Dislikes: 173
+ * Total Accepted:    86K
+ * Total Submissions: 246.1K
+ * Testcase Example:  '[3,6,9,1]'
+ *
+ * Given an unsorted array, find the maximum difference between the successive
+ * elements in its sorted form.
+ * 
+ * Return 0 if the array contains less than 2 elements.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [3,6,9,1]
+ * Output: 3
+ * Explanation: The sorted form of the array is [1,3,6,9], either
+ * (3,6) or (6,9) has the maximum difference 3.
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [10]
+ * Output: 0
+ * Explanation: The array contains less than 2 elements, therefore return 0.
+ * 
+ * Note:
+ * 
+ * 
+ * You may assume all elements in the array are non-negative integers and fit
+ * in the 32-bit signed integer range.
+ * Try to solve it in linear time/space.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    private class Bucket {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+    }
+    public int maximumGap(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return 0;
+        }
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i : nums) {
+            min = Math.min(min, i);
+            max = Math.max(max, i);
+        }
+        int bucketSize = Math.max(1, (max - min) / (nums.length - 1));
+        Bucket[] buckets = new Bucket[(max - min) / bucketSize + 1];
+        for (int i = 0; i < nums.length; i++) {
+            int loc = (nums[i] - min) / bucketSize;
+            if (buckets[loc] == null) {
+                buckets[loc] = new Bucket();
+            }
+            buckets[loc].min = Math.min(buckets[loc].min, nums[i]);
+            buckets[loc].max = Math.max(buckets[loc].max, nums[i]);
+        }
+        int previousMax = Integer.MAX_VALUE;
+        int maxGap = Integer.MIN_VALUE;
+        for (int i = 0; i < buckets.length; i++) {
+            if (buckets[i] != null && previousMax != Integer.MAX_VALUE) {
+                maxGap = Math.max(maxGap, buckets[i].min - previousMax);
+            }
+            if (buckets[i] != null) {
+                previousMax = buckets[i].max;
+                maxGap = Math.max(maxGap, buckets[i].max - buckets[i].min);
+            }
+        }
+        return maxGap;
+    }
+}
+// @lc code=end
+
